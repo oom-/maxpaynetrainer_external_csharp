8f79fe8aaad46eb463d617c586688882ddd0099d
webwood <webwoodme@gmail.com>
2018-12-17 22:21:23 +0100

    Ajoutez des fichiers projet.

---
+++ MaxPayneExternalTrainer/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxPayneExternalTrainer
+{
+    public partial class Form1 : Form
+    {
+        MemUtil mu;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mu = new MemUtil();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
