fe15bff98d42283b257482ba56f635d3f9575135
oom- <11071671+oom-@users.noreply.github.com>
2018-12-17 22:21:23 +0100

    Ajoutez des fichiers projet.

---
+++ MaxPayneExternalTrainer/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MaxPayneExternalTrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Infinite Life";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(166, 25);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(150, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Infinite Slow motion";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(26, 77);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(114, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Infinite Ammo";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 179);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ MaxPayneExternalTrainer/MemUtil.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxPayneExternalTrainer
+{
+    class MemUtil
+    {
+        #region NativeFunctions
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, ulong dwSize, out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, ulong dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        const int PROCESS_QUERY_INFORMATION = 0x400;
+        const int PROCESS_VM_OPERATION = 0x8;
+        const int PROCESS_VM_READ = 0x10;
+        const int PROCESS_VM_WRITE = 0x20;
+        #endregion
+
+        Process m_p;
+        IntPtr m_handle;
+
+        public bool OpenHandle(string name)
+        {
+            m_p = Process.GetProcessesByName(name).FirstOrDefault();
+            if (m_p != null)
+            {
+                m_handle = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION, false, m_p.Id);
+            }
+            return false;
+        }
+
+        private byte[] ReadMemory(IntPtr baseAddr, ulong memorySize, out int readed)
+        {
+            if (memorySize > 2147483648) //Not abuse
+            {
+                readed = 0;
+                return new byte[0];
+            }
+
+            byte[] memoryData = new byte[memorySize];
+            ReadProcessMemory(m_handle, baseAddr, memoryData, memorySize, out readed);
+            return memoryData;
+        }
+
+        public int WritePatternFromAddr(IntPtr addr, byte[] pattern)
+        {
+            int writen = 0;
+            WriteProcessMemory(m_handle, addr, pattern, (ulong)pattern.Length, ref writen);
+            return writen;
+        }
+
+        public IntPtr GetMainModuleAdress()
+        {
+            if (m_p != null)
+                return m_p.MainModule.BaseAddress;
+            return IntPtr.Zero;
+        }
+
+        public bool CloseHandle()
+        {
+            if (m_handle != null)
+            {
+               return CloseHandle(m_handle);
+            }
+            return true; //no handle then closed yep
+        }
+    }
+}
