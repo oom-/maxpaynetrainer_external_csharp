4f7e3d5bc87fd44b1c30f710a5ba83504961567a
oom <venom0218@gmail.com>
2018-12-21 00:15:53 +0100

    Add module scan and painkiller

--- MaxPayneExternalTrainer/Form1.Designer.cs
+++ MaxPayneExternalTrainer/Form1.Designer.cs
@@ -34,15 +34,17 @@
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Enabled = false;
-            this.checkBox1.Location = new System.Drawing.Point(26, 25);
+            this.checkBox1.Location = new System.Drawing.Point(20, 20);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "Infinite Life";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -52,9 +54,10 @@
             // 
             this.checkBox2.AutoSize = true;
             this.checkBox2.Enabled = false;
-            this.checkBox2.Location = new System.Drawing.Point(166, 25);
+            this.checkBox2.Location = new System.Drawing.Point(124, 20);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(150, 21);
+            this.checkBox2.Size = new System.Drawing.Size(117, 17);
             this.checkBox2.TabIndex = 1;
             this.checkBox2.Text = "Infinite Slow motion";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -64,9 +67,10 @@
             // 
             this.checkBox3.AutoSize = true;
             this.checkBox3.Enabled = false;
-            this.checkBox3.Location = new System.Drawing.Point(26, 77);
+            this.checkBox3.Location = new System.Drawing.Point(20, 63);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(114, 21);
+            this.checkBox3.Size = new System.Drawing.Size(89, 17);
             this.checkBox3.TabIndex = 2;
             this.checkBox3.Text = "Infinite Ammo";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -75,9 +79,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 153);
+            this.label1.Location = new System.Drawing.Point(9, 124);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 17);
+            this.label1.Size = new System.Drawing.Size(137, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Looking for maxpayne.exe..";
             // 
@@ -86,15 +91,30 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(124, 63);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(103, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Infinite PainKiller";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 179);
+            this.ClientSize = new System.Drawing.Size(316, 145);
+            this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -110,6 +130,7 @@
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox4;
     }
 }
 
